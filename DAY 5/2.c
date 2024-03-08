0d11bd4ac9dbb7bf8c70ad0fa38ea6ccd778fc04
KartikChavda123 <kartikchavda2005@gmail.com>
2024-03-08 21:41:35 +0530

    C work

--- DAY 5/2.c
+++ DAY 5/2.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 int main(){
-    
+    int a,b;
+
+    printf("Enter The Value Of a=");
+    scanf("%d",&a);
+
+    printf("Enter The Value Of b=");
+    scanf("%d",&b);
+
+    if (a > b)
+    {
+        printf("A is big %d",a);
+    }
+    else{
+        printf("B is big %d",b);
+    }
 }
